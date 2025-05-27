7261bbeeddfc4550e41db10bf557d06df6ee891d
Mohit Yadav <mohitkyadav@google.com>
2025-05-27 02:23:14 +0000

    add comments command for benchmark 1

--- perf-benchmarking-for-releases/run-benchmarks.sh
+++ perf-benchmarking-for-releases/run-benchmarks.sh
@@ -27,6 +27,10 @@ if [ "$#" -ne 6 ]; then
     exit 1
 fi
 
+# #1 Benchmark
+# bash run run-benchmarks.sh change-default-configs-for-streaming-writes gcs-fuse-test us-south1 c4-standard-96 ubuntu-2004-lts ubuntu-os-cloud
+# bash create_benchmark_tables.sh change-default-configs-for-streaming-writes us-south1 c4-standard-96 'gVNIC+ tier_1 networking (200Gbps)' 'Hyperdisk balanced'
+
 # Print commands and their arguments as they are executed.
 set -x
 # Exit immediately if a command exits with a non-zero status.
