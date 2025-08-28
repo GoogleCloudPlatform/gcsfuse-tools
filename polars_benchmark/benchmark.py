21ee788f667d97f6aae7430a2758c63eebc517d6
Kislay Kishore <kislayk@google.com>
2025-08-28 08:56:24 +0530

    Create directories

--- polars_benchmark/benchmark.py
+++ polars_benchmark/benchmark.py
@@ -17,6 +17,7 @@ def _generate_dummy_dataframe(num_rows: int) -> pd.DataFrame:
 
 def _write_single_file(df: pl.DataFrame, file_path: str):
     """Helper to write a single parquet file."""
+    os.makedirs(os.path.dirname(file_path), exist_ok=True)
     df.write_parquet(file_path)
 
 def run_write_benchmark(df: pl.DataFrame, base_file_path: str, nr_files: int, num_runs: int = 5) -> list[float]:
