2a90b5c44dd8ecb3a1d4e0225489f0bb2260c568
alleaditya <alleaditya@google.com>
2025-09-30 16:32:29 +0530

    Test changes for fio

--- gcsfuse-micro-benchmarking/helpers/constants.py
+++ gcsfuse-micro-benchmarking/helpers/constants.py
@@ -1,13 +1,13 @@
-default_bs="4KB"
+default_bs="16MB"
 default_file_size="1GB"
 default_iodepth=1
-default_iotype="read"
+default_iotype="randread"
 default_threads=1
 default_nrfiles=1
 
 default_go_version="1.24.0"
 default_fio_version="3.39"
-default_gcsfuse_version="master"
+default_gcsfuse_version="enable_default_o_direct"
 
 
 max_ssh_retries=5
@@ -16,14 +16,14 @@ poll_interval=60
 timeout=180000
 
 
-default_machine_type="n2-standard-16"
+default_machine_type="c4-standard-96"
 default_image_family="debian-11"
 default_image_project="debian-cloud"
-default_disk_size="200GB"
+default_disk_size="500GB"
 default_startup_script="./resources/starter_script.sh"
 default_delete_after_use=False
-default_zone="us-central1-a"
-default_region="us-central1"
+default_zone="us-west4-a"
+default_region="us-west4"
 default_enable_hns=False
 default_project="gcs-fuse-test"
 
@@ -34,4 +34,4 @@ default_mount_config_file=".resources/mount_config.yml"
 default_fio_jobcases_file="./resources/fio_job_cases.csv"
 
 
-default_iterations=5
+default_iterations=3
