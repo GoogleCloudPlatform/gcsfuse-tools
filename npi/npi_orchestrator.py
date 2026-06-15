89a2b57e81926dfbb016a8ca3d82c6c09c570a21
Kislay Kishore <kislayk@google.com>
2026-06-15 14:32:44 +0530

    perf(npi): reduce log inactivity check from 60 mins to 5 mins

--- npi/npi_orchestrator.py
+++ npi/npi_orchestrator.py
@@ -280,7 +280,7 @@ def monitor_run(target, socket_path, state_lock, state):
 
     last_log_change_time = time.time()
     previous_log_line = ""
-    MAX_INACTIVITY_SECS = 600
+    MAX_INACTIVITY_SECS = 300
     
     consecutive_ssh_failures = 0
     MAX_SSH_RETRIES = 3
